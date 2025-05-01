5a018b262d43f50a5acb0cc54bfd1e554264cec1
Frontend <117911062+Himanshikhare@users.noreply.github.com>
2025-05-01 21:01:44 +0530

    Fix build issue with Signup component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/shared/Navbar'
 import Home from './components/pages/Home'
 
-import Signup from './components/auth/signup'
+import Signup from './components/auth/Signup'
 import Login from './components/auth/login'
 import Services from './components/pages/Services'
 import About from './components/pages/About'
